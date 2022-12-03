29cacc7b0f3666f35facb545c85b5a7476f519b8
Lolita <66422533+Mazharova@users.noreply.github.com>
2022-12-03 17:10:55 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Main5.MovableCircle movableCircle = new Main5.MovableCircle(10, 0, 0,1, 1);
+        System.out.println(movable_circle);
+        movable_circle.moveUp();
+        movable_circle.moveUp();
+        movable_circle.moveRight();
+        System.out.println(movable_circle);
+        movable_circle.moveDown();
+        movable_circle.moveDown();
+        movable_circle.moveDown();
+        movable_circle.moveDown();
+        System.out.println(movable_circle);
+    }
+}
